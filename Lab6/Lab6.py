43f2766c6759b6d44198208b357cf8c96cd53abc
s01158158 <39176224+s01158158@users.noreply.github.com>
2019-10-06 23:37:45 -0400

    Create Lab6.py

---
+++ Lab6/Lab6.py
@@ -0,0 +1,15 @@
+i=0 # 0 to 50 by 5
+>>> while i <= 50:
+    print(i)
+    i += 5
+
+
+
+a = "racecar"
+b = "" 
+while i in a: 
+    b = i + b 
+    if (a==b): 
+        print("True")
+    else:
+        print("False")
