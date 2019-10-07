67f6af7fd7a7ed3041aa28bf2a58ff4d2404d1a4
s01158158 <39176224+s01158158@users.noreply.github.com>
2019-10-06 23:57:19 -0400

    Update Lab6.py

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -3,7 +3,12 @@ i=0 # 0 to 50 by 5
     print(i)
     i += 5
 
-
+grades = []
+newgrade = ''
+while newgrade != 'stop':
+    newgrade = input("Please enter Grade, or enter 'stop': ")
+    grades.append(newgrade)
+    print(grades)
 
 a = "racecar"
 b = "" 
